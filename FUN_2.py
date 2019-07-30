e34b8eeeb98b9d1fc4dd7c7b56831be2c16cc1cb
orishahar21-meet <orishahar21@meet.mit.edu>
2019-07-30 16:56:17 +0300

    Stopping here

--- FUN_2.py
+++ FUN_2.py
@@ -3,12 +3,30 @@ turtle.goto(0,0)
 
 turtle.direction = None
 def up():
-    turtle.forward(10)
+    x,y = turtle.pos()
+    turtle.goto(x,y+50)
     print("you pressed the up key")
-    turtle.direction = "Up"
 
 turtle.onkey(up, "Up")
 turtle.listen()
 
 def down():
+    x, y = turtle.pos()
+    turtle.goto(x,y-50)
+
+turtle.onkey(down, "Down")
+turtle.listen()
     
+def left():
+    x,y = turtle.pos()
+    turtle.goto(x-50,y)
+
+turtle.onkey(left, "Left")
+turtle.listen()
+
+def right():
+    x, y = turtle.pos()
+    turtle.goto(x+50, y)
+
+turtle.onkey(right, "Right")
+turtle.listen()
